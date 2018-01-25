8e3a0e6fe2e683d19d888080ceb990647361b2e2
Justin E. Green <33797772+J-E-G@users.noreply.github.com>
2018-01-25 12:47:19 -0600

    Add new activity to display score result on Submit button press.

--- app/src/main/java/com/example/android/scrumquiz/MainActivity.java
+++ app/src/main/java/com/example/android/scrumquiz/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.android.scrumquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
+public class MainActivity extends AppCompatActivity {
+    int score = 99;
+    public static final String EXTRA_MESSAGE = "com.example.android.scrumquiz.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +21,15 @@ public class MainActivity extends AppCompatActivity {
     // This method is called when the Submit button is pressed.
     public void scoreQuiz(View view) {
         calculateQuizScore();
+        Intent intent = new Intent(this, DisplayScoreActivity.class);
+        String message = "You scored " + score + " by answering 5 out of 5 questions correctly.";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     // This method calculates the users total score on the quiz.
     private void calculateQuizScore() {
-        int score = 100;
-        Toast.makeText(this, "You scored " + score + " by answering 5 out of 5 questions correctly.", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "New Activity Triggered", Toast.LENGTH_SHORT).show();
     }
 }
