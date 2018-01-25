b83642141a4bb7b74a2715c04bf921671cb2b8fc
Justin E. Green <33797772+J-E-G@users.noreply.github.com>
2018-01-24 22:14:08 -0600

    Update layout style. Create shell method for submit button.

--- app/src/main/java/com/example/android/scrumquiz/MainActivity.java
+++ app/src/main/java/com/example/android/scrumquiz/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.scrumquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // This method is called when the Submit button is pressed.
+    public void scoreQuiz(View view) {
+        calculateQuizScore();
+    }
+
+    // This method calculates the users total score on the quiz.
+    private void calculateQuizScore() {
+        int score = 100;
+        Toast.makeText(this, "You scored " + score + " by answering 5 out of 5 questions correctly.", Toast.LENGTH_SHORT).show();
     }
 }
