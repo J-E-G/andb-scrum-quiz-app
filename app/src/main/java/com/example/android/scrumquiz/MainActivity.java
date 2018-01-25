a618ce43c23af24695bfebbdc98013a10c2f79e3
Justin E. Green <33797772+J-E-G@users.noreply.github.com>
2018-01-25 13:26:16 -0600

    Add score method for question one.

--- app/src/main/java/com/example/android/scrumquiz/MainActivity.java
+++ app/src/main/java/com/example/android/scrumquiz/MainActivity.java
@@ -4,14 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-    int score = 99;
+    int score = 0;
+    int correctAnswers = 0;
     public static final String EXTRA_MESSAGE = "com.example.android.scrumquiz.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +18,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // This method is called when the Submit button is pressed.
-    public void scoreQuiz(View view) {
+    public void showQuizScore(View view) {
         calculateQuizScore();
         Intent intent = new Intent(this, DisplayScoreActivity.class);
-        String message = "You scored " + score + " by answering 5 out of 5 questions correctly.";
+        String message = "You scored " + score + " points by answering " + correctAnswers + " out of 5 questions correctly.";
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
     // This method calculates the users total score on the quiz.
     private void calculateQuizScore() {
+        RadioButton question1_answer3 = findViewById(R.id.question1_answer3);
 
-        Toast.makeText(this, "New Activity Triggered", Toast.LENGTH_SHORT).show();
+        if (question1_answer3.isChecked()) {
+            score += 25;
+            correctAnswers += 1;
+        }
     }
+
 }
