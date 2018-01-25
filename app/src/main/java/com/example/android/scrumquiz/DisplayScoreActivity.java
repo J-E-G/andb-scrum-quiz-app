52f5dbb31360f26a9f8cc0aed64aadb33e56bf58
Justin E. Green <33797772+J-E-G@users.noreply.github.com>
2018-01-25 16:30:37 -0600

    Finish scoring method and update UI.

--- app/src/main/java/com/example/android/scrumquiz/DisplayScoreActivity.java
+++ app/src/main/java/com/example/android/scrumquiz/DisplayScoreActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.scrumquiz;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DisplayScoreActivity extends AppCompatActivity {
@@ -19,5 +23,11 @@ public class DisplayScoreActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+    }
+
+    public  void showMoreInfo(View view) {
+        Uri uri = Uri.parse("http://www.scrumguides.org/scrum-guide.html");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android/scrumquiz/MainActivity.java
+++ app/src/main/java/com/example/android/scrumquiz/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,19 +30,55 @@ public class MainActivity extends AppCompatActivity {
 
     // This method calculates the users total score on the quiz.
     private void calculateQuizScore() {
-        int addPoints = 25;
-        RadioButton question1_answer3 = findViewById(R.id.question1_answer3);
-        RadioButton question2_answer2 = findViewById(R.id.question2_answer2);
+        score = 0; // reset the value back to 0 after user comes back from score view activity
+        correctAnswers = 0;
+        int addPoints = 20;
+        RadioButton question1_answer3 = findViewById(R.id.question1_answer3); // Question 1 Answer
+        RadioButton question2_answer2 = findViewById(R.id.question2_answer2); // Question 2 Answer
 
+        /* Question 3 Answers */
+        CheckBox question3_answer1 = findViewById(R.id.question3_answer1);
+        CheckBox question3_answer3 = findViewById(R.id.question3_answer3);
+        CheckBox question3_answer4 = findViewById(R.id.question3_answer4);
+
+        // Question 4 User Input
+        EditText question4_user_input = findViewById(R.id.question4_answer_input);
+        String question4_answer = question4_user_input.getText().toString();
+
+        RadioButton question5_answer2 = findViewById(R.id.question5_answer2); // Question 5 Answer
+
+        // If Question 1 is correct, increment points and correct answer count
         if (question1_answer3.isChecked()) {
             score += addPoints;
             correctAnswers += 1;
         }
 
+        // If Question 2 is correct, increment points and correct answer count
         if (question2_answer2.isChecked()) {
             score += addPoints;
             correctAnswers += 1;
         }
+
+        /* If Question 3 has the correct checkboxes selected, increment points and correct answer count.
+        Points are only incremented on selection of the correct set of checkboxes */
+
+        if (question3_answer1.isChecked() && question3_answer3.isChecked() && question3_answer4.isChecked()) {
+            score += addPoints;
+            correctAnswers += 1;
+        }
+
+        // If Question 4 is correct, increment points and correct answer count.
+        if (question4_answer.equalsIgnoreCase(getString(R.string.question_4_answer))) {
+            score += addPoints;
+            correctAnswers += 1;
+        }
+
+        // If Question 5 is correct, increment points and correct answer count.
+        if (question5_answer2.isChecked()) {
+            score += addPoints;
+            correctAnswers += 1;
+        }
+
     }
 
 }
